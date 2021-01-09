c1cd81e9a42ce914601f37297e29cd8c6f40d4be
Dennis Liang <dliang2248@student.pps.net>
2021-01-08 17:06:29 -0800

    Final  commit

---
+++ server.js
@@ -0,0 +1,14 @@
+let http = require("http"); 
+let fs = require("fs"); 
+
+console.log("Hello  Running at 8080");
+http.createServer((req, res) =>{
+
+    fs.readFile("web/index.html", (err, data)=>{
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.write(data); 
+        return res.end();
+    }); 
+
+}).listen(process.env.PORT || 8000);
+
