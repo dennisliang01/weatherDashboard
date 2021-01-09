9136047f0667e116c7ed7cc15592180a4db5d985
Dennis Liang <dennisliang01@gmail.com>
2021-01-08 17:26:57 -0800

    fixing bug again

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ let fs = require("fs");
 console.log("Hello  Running at 8080");
 http.createServer((req, res) =>{
 
-    fs.readFile("server/index.html", (err, data)=>{
+    fs.readFile("public/index.html", (err, data)=>{
         res.writeHead(200, {"Content-Type": "text/html"});
         res.write(data); 
         return res.end();
-    }); 
+    });
+
+
 
 }).listen(process.env.PORT || 8000);
 
